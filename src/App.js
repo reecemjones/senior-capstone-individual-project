70223c9f12b05ca7ebaf549c3f22f2aee821b682
reecemjones15 <reecemjones15@gmail.com>
2021-10-04 11:28:28 -0400

    Change title

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <h1>Individual Project | Data Visualizations</h1>
+    </div>
+  );
 }
 
 export default App;
