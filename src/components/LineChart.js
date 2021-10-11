8c7ce3d01a97183261e0347484d75f1eeb9e6c5c
reecemjones15 <reecemjones15@gmail.com>
2021-10-10 20:53:19 -0400

    Add x and y axis/legends

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -7,18 +7,50 @@ function LineChart(props) {
   }, []);
 
   function drawChart() {
+    const data = props.data;
+
+    // create initial svg element
     const svg = d3.select(`#${props.id}`).append("svg");
+
+    // create xScale & yScale range
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, data.length - 1]) // data
+      .range([0, 300]); // length in pixels
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, Math.max(...data)]) // data
+      .range([150, 0]); // height in pixels
+
+    // append x & y axis labels
+    svg
+      .append("g")
+      .style("transform", "translateY(150px)")
+      .call(
+        d3
+          .axisBottom(xScale)
+          .ticks(data.length)
+          .tickFormat((index) => index + 1)
+      );
+    svg
+      .append("g")
+      .style("transform", "translateX(300px)")
+      .call(d3.axisRight(yScale));
+
+    // create line points
     const myLine = d3
       .line()
-      .x((value, index) => index * 50)
-      .y((value) => 150 - value)
+      .x((value, index) => xScale(index))
+      .y(yScale)
       .curve(d3.curveCardinal);
 
+    // append line points to svg as a stroke path
     svg
-      .selectAll("path")
-      .data([props.data])
+      .selectAll(".line")
+      .data([data])
       .join("path")
-      .attr("d", (value) => myLine(value))
+      .attr("class", "line")
+      .attr("d", myLine)
       .attr("fill", "none")
       .attr("stroke", "royalblue");
   }
